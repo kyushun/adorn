9f018b17acec1108576ec032c423d02d8e0ee76a
kyushun <27068326+kyushun@users.noreply.github.com>
2022-07-18 22:38:11 +0900

    refacotr

--- components/index-page/index-page.tsx
+++ components/index-page/index-page.tsx
@@ -1,37 +1,7 @@
-import { useEffect, useRef } from "react";
-import useSWRInfinite from "swr/infinite";
-import { GetPostsResponse } from "utils/type";
-
-import { useScrollToBottom } from "./use-scroll-to-bottom";
+import { useFetchPosts } from "./use-fetch-posts";
 
 export const IndexPage = () => {
-  const { data, error, size, setSize } = useSWRInfinite<GetPostsResponse>(
-    (index, prev) => {
-      if (prev && prev.posts.length === 0) return null;
-
-      if (index === 0) return "/api/images";
-
-      return `/api/images?cursor=${prev!.cursor}`;
-    }
-  );
-  const [bottomRef, isBottom] = useScrollToBottom();
-
-  const isReachingEndRef = useRef(false);
-  isReachingEndRef.current =
-    (data && data[data.length - 1].posts.length === 0) || false;
-
-  const isLoadingInitialData = !data && !error;
-  const isLoadingMore =
-    isLoadingInitialData ||
-    (size > 0 && data && typeof data[size - 1] === "undefined");
-
-  useEffect(() => {
-    if (!isBottom) return;
-    if (isReachingEndRef.current) return;
-
-    setSize((size) => size + 1);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isBottom]);
+  const { bottomRef, data, error, isLoadingMore } = useFetchPosts();
 
   if (error) {
     return <div>error</div>;
@@ -52,7 +22,7 @@ export const IndexPage = () => {
                 key={image.url}
                 src={image.url}
                 alt=""
-                className="mb-4 rounded-xl"
+                className="mb-4 cursor-pointer rounded-xl transition-opacity hover:opacity-75"
               />
             ))
           )
---
+++ components/index-page/use-fetch-posts.ts
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from "react";
+import useSWRInfinite from "swr/infinite";
+import { GetPostsResponse } from "utils/type";
+
+import { useScrollToBottom } from "./use-scroll-to-bottom";
+
+export const useFetchPosts = () => {
+  const { data, error, size, setSize } = useSWRInfinite<GetPostsResponse>(
+    (index, prev) => {
+      if (prev && prev.posts.length === 0) return null;
+
+      if (index === 0) return "/api/images";
+
+      return `/api/images?cursor=${prev!.cursor}`;
+    }
+  );
+  const [bottomRef, isBottom] = useScrollToBottom();
+
+  const isReachingEndRef = useRef(false);
+  isReachingEndRef.current =
+    (data && data[data.length - 1].posts.length === 0) || false;
+
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore =
+    isLoadingInitialData ||
+    (size > 0 && data && typeof data[size - 1] === "undefined");
+
+  useEffect(() => {
+    if (!isBottom) return;
+    if (isReachingEndRef.current) return;
+
+    setSize((size) => size + 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isBottom]);
+
+  return {
+    bottomRef,
+    data,
+    error,
+    isLoadingInitialData,
+    isLoadingMore,
+  };
+};
