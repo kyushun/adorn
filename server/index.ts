55a94f5fe7850b88b9723b8be7fe5f98b80c9475
kyushun <27068326+kyushun@users.noreply.github.com>
2022-07-18 14:50:02 +0900

    images api

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import express, { Request, Response } from "express";
 import next from "next";
 
 import apiRouter from "./api";
+import imagesRouter from "./images";
 
 const dev = process.env.NODE_ENV === "development";
 const port = 3000;
@@ -18,6 +19,7 @@ const handle = app.getRequestHandler();
     const server = express();
 
     server.use("/api", apiRouter);
+    server.use("/images", imagesRouter);
 
     server.all("*", (req: Request, res: Response) => {
       return handle(req, res);
