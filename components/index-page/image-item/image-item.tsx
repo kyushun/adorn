a91f519a7b3ec005f8a26e0928bfcea790e62767
kyushun <27068326+kyushun@users.noreply.github.com>
2022-07-22 17:54:29 +0900

    投稿者のプロフィールに飛べるようにする

--- components/index-page/image-item/image-item.tsx
+++ components/index-page/image-item/image-item.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import { Image, Post } from "utils/type";
 
+import { createTwitterProfileUrl } from "@/server/utils/url";
+
 export const ImageItem = ({
   post,
   image,
@@ -14,17 +16,36 @@ export const ImageItem = ({
 }) => {
   return (
     <div
-      className="relative m-1 cursor-pointer overflow-hidden rounded-xl transition-opacity hover:opacity-80"
+      className="group relative m-1 cursor-pointer overflow-hidden rounded-xl"
       style={{ transform: "translate3d(0, 0, 0)" }}
       onClick={() => {
         onClick(post.id, image.url);
       }}
     >
       <div>
-        <img key={image.url} src={image.url} alt="" onLoad={onLoad} />
+        <img
+          key={image.url}
+          src={image.url}
+          alt=""
+          onLoad={onLoad}
+          className="transition-opacity group-hover:opacity-80"
+        />
 
         <div className="absolute bottom-0 left-0 w-full bg-gradient-to-b from-transparent to-gray-800/75 px-4 pt-8 pb-4 text-gray-50">
-          <p className="text-sm font-bold line-clamp-1">{post.user.name}</p>
+          <a
+            href={createTwitterProfileUrl(post.user.id)}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p
+              className="-mx-2 rounded px-2 text-sm font-bold transition-colors line-clamp-1 hover:bg-black/20"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              {post.user.name}
+            </p>
+          </a>
           <p className="text-sm line-clamp-3">
             {post.text.replace(/https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+$/, "")}
           </p>
